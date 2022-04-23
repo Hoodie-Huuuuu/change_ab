3e1de96d39bb0be14e06fa2d08797d1787931c4f
George Shpilevoy <georgeshpilevoy@GogPro.local>
2022-04-23 21:14:13 +0300

    setup is changed

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 import setuptools
 
+packs = setuptools.find_packages()
+print(packs)
+
 setuptools.setup(
     name='change_ab',
     version='0.0.1',
     author='George Shpilevoy',
     description='Testing installation of Package',
     url='https://github.com/Hoodie-Huuuuu/change_ab',
-    packages=setuptools.find_packages()
+    packages=packs
 )
