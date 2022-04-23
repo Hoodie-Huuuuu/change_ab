6cae10497211b084f052e48f8116233fd041a202
George Shpilevoy <georgeshpilevoy@GogPro.local>
2022-04-23 20:20:19 +0300

    version is changed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='change_ab',
-    version='0.0.0',
+    version='0.0.1',
     author='George Shpilevoy',
     description='Testing installation of Package',
     url='https://github.com/Hoodie-Huuuuu/change_ab',
