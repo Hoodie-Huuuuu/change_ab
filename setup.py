1f7a786d5b406165282f7d4b34179d6d16af3cf8
George Shpilevoy <georgeshpilevoy@GogPro.local>
2022-04-23 19:13:12 +0300

    setup.py is added

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='change_ab',
+    version='0.0.0',
+    author='George Shpilevoy',
+    description='Testing installation of Package',
+    url='https://github.com/Hoodie-Huuuuu/change_ab',
+    packages=['change_ab'],
+    install_requires=['os'],
+)
